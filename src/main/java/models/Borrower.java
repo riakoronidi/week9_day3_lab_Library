743717734f4197ad8b147eaa6e597643c1a2a148
Ed Ostler <edward.o@simul8.com>
2018-03-21 16:30:23 +0000

    Adds borrower class declarations, constructors and getter/setters as well as test for getName

--- src/main/java/models/Borrower.java
+++ src/main/java/models/Borrower.java
@@ -1,4 +1,40 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Borrower {
+
+    private int id;
+    private String name;
+    private List<Book> borrowedBooks;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.borrowedBooks = new ArrayList();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public void setBorrowedBooks(List<Book> borrowedBooks) {
+        this.borrowedBooks = borrowedBooks;
+    }
 }
--- src/test/java/models/BorrowerTest.java
+++ src/test/java/models/BorrowerTest.java
@@ -1,4 +1,22 @@
 package models;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+
+    private Borrower borrower;
+
+    @Before
+    public void before() {
+        borrower = new Borrower("Ed");
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Ed", borrower.getName());
+    }
+
 }
